1c7b90a8fabd1df44fba16a88e55c17fe8f96609
Nikhil Singh <nikhilsingh26042004@gmail.com>
2024-12-20 16:58:23 +0530

    created Operate function

---
+++ script.js
@@ -0,0 +1,33 @@
+let num1;
+let num2;
+let operator;
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(num1, operator, num2) {
+  if (operator == "+") {
+    return add(num1, num2);
+  } else if (operator == "-") {
+    return subtract(num1, num2);
+  } else if (operator == "*") {
+    return multiply(num1, num2);
+  } else if (operator == "/") {
+    return divide(num1, num2);
+  } else {
+    alert("enter a valid operator");
+  }
+}
