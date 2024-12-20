46e23ab487490e5d2ca83ace0b61e9aecb8cde55
Nikhil Singh <nikhilsingh26042004@gmail.com>
2024-12-20 21:04:52 +0530

    created button's working

--- script.js
+++ script.js
@@ -31,3 +31,120 @@ function operate(num1, operator, num2) {
     alert("enter a valid operator");
   }
 }
+
+
+
+
+let displaybox = document.querySelector(".displaybox");
+let output = "";
+
+// let ac=document.querySelector(".ac")
+// ac.addEventListener("click")
+
+// let signAddMin=document.querySelector(".signAddMin")
+// signAddMin.addEventListener("click")
+
+let percentage=document.querySelector(".percentage")
+percentage.addEventListener("click",()=>{
+    output += "%";
+    displaybox.innerHTML = output;
+})
+
+let division=document.querySelector(".division")
+division.addEventListener("click",()=>{
+    output += "/";
+    displaybox.innerHTML = output;
+})
+
+let seven = document.querySelector(".vii");
+seven.addEventListener("click", () => {
+  output += "7";
+  displaybox.innerHTML = output;
+});
+
+let eight = document.querySelector(".viii");
+eight.addEventListener("click", () => {
+  output += "8";
+  displaybox.innerHTML = output;
+});
+
+let nine = document.querySelector(".ix");
+nine.addEventListener("click", () => {
+  output += "9";
+  displaybox.innerHTML = output;
+});
+
+let multiplication=document.querySelector(".multiplication")
+multiplication.addEventListener("click",()=>{
+    output += "*";
+    displaybox.innerHTML = output;
+})
+
+let four = document.querySelector(".iv");
+four.addEventListener("click", () => {
+  output += "4";
+  displaybox.innerHTML = output;
+});
+
+let five = document.querySelector(".v");
+five.addEventListener("click", () => {
+  output += "5";
+  displaybox.innerHTML = output;
+});
+
+let six = document.querySelector(".vi");
+six.addEventListener("click", () => {
+  output += "6";
+  displaybox.innerHTML = output;
+});
+
+let subtraction=document.querySelector(".subtraction")
+subtraction.addEventListener("click",()=>{
+    output += "-";
+    displaybox.innerHTML = output;
+})
+
+let one = document.querySelector(".i");
+one.addEventListener("click", () => {
+  output += "1";
+  displaybox.innerHTML = output;
+});
+
+let two = document.querySelector(".ii");
+two.addEventListener("click", () => {
+  output += "2";
+  displaybox.innerHTML = output;
+});
+
+let three = document.querySelector(".iii");
+three.addEventListener("click", () => {
+  output += "3";
+  displaybox.innerHTML = output;
+});
+
+let addition=document.querySelector(".addition")
+addition.addEventListener("click",()=>{
+    output += "+";
+    displaybox.innerHTML = output;
+})
+
+// let calc=document.querySelector(".calc")
+// calc.addEventListener("click")
+
+let zero = document.querySelector(".zero");
+zero.addEventListener("click", () => {
+  output += "0";
+  displaybox.innerHTML = output;
+});
+
+let decimal=document.querySelector(".decimal")
+decimal.addEventListener("click",()=>{
+    output += ".";
+    displaybox.innerHTML = output;
+})
+
+let isEqualTo=document.querySelector(".isEqualTo")
+isEqualTo.addEventListener("click",()=>{
+    output += "=";
+    displaybox.innerHTML = output;
+})
