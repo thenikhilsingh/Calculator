dc6f8048ccfff3e8deff2104260fe581a0d8050a
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2024-12-22 01:37:47 +0530

    bugs fixed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let arg1 = 0;
 let arg2 = 0;
 let operand = 0;
+let store = 0;
 let bksp = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>backspace-outline</title><path d="M19,15.59L17.59,17L14,13.41L10.41,17L9,15.59L12.59,12L9,8.41L10.41,7L14,10.59L17.59,7L19,8.41L15.41,12L19,15.59M22,3A2,2 0 0,1 24,5V19A2,2 0 0,1 22,21H7C6.31,21 5.77,20.64 5.41,20.11L0,12L5.41,3.88C5.77,3.35 6.31,3 7,3H22M22,5H7L2.28,12L7,19H22V5Z" /></svg>`;
 let opflag = false;
 
@@ -24,19 +25,25 @@ function remainder(num1, num2) {
   return num1 % num2;
 }
 
-function operate(num1, operator, num2) {
+function calculate(num1, operator, num2) {
   if (operator == "+") {
-    return (displaybox.innerHTML = add(num1, num2));
+    output = add(num1, num2);
+    displaybox.innerHTML = output;
   } else if (operator == "-") {
-    return (displaybox.innerHTML = subtract(num1, num2));
+    output = subtract(num1, num2);
+    displaybox.innerHTML = output;
   } else if (operator == "*") {
-    return (displaybox.innerHTML = multiply(num1, num2));
+    output = multiply(num1, num2);
+    displaybox.innerHTML = output;
   } else if (operator == "/") {
-    return (displaybox.innerHTML = divide(num1, num2));
+    output = divide(num1, num2);
+    displaybox.innerHTML = output;
   } else if (operator == "%") {
-    return (displaybox.innerHTML = remainder(num1, num2));
+    output = remainder(num1, num2);
+    displaybox.innerHTML = output;
   } else {
-    return (displaybox.innerHTML = "enter a valid input");
+    output = "enter a valid input";
+    displaybox.innerHTML = output;
   }
 }
 
@@ -50,12 +57,30 @@ let ac = document.querySelector(".ac");
 let acflag = false;
 ac.addEventListener("click", () => {
   if (acflag) {
+    opflag = false;
+    decflag = false;
     output = "";
     displaybox.innerHTML = 0;
     displayAfter.innerHTML = "";
   } else {
+    if (
+      output[output.length - 1] == "+" ||
+      output[output.length - 1] == "-" ||
+      output[output.length - 1] == "*" ||
+      output[output.length - 1] == "/" ||
+      output[output.length - 1] == "%" ||
+      output[output.length - 1] == "."
+    ) {
+      opflag = false;
+      decflag = false;
+    }
+
     output = output.slice(0, -1);
     displaybox.innerHTML = output;
+    if (output == "") {
+      acflag = true;
+      ac.innerHTML = "AC";
+    }
   }
 });
 
@@ -74,12 +99,14 @@ signAddMin.addEventListener("click", () => {
 let modulus = document.querySelector(".modulus");
 modulus.addEventListener("click", () => {
   if (opflag) {
+    operate();
     acflag = false;
-    ac.innerHTML = bksp;
+    operand = "%";
+    output += "%";
     displaybox.innerHTML = output;
   } else {
     opflag = true;
-    acflag = false;
+    decflag = true;
     ac.innerHTML = bksp;
     operand = "%";
     output += "%";
@@ -90,12 +117,14 @@ modulus.addEventListener("click", () => {
 let division = document.querySelector(".division");
 division.addEventListener("click", () => {
   if (opflag) {
+    operate();
     acflag = false;
-    ac.innerHTML = bksp;
+    operand = "/";
+    output += "/";
     displaybox.innerHTML = output;
   } else {
     opflag = true;
-    acflag = false;
+    decflag = true;
     ac.innerHTML = bksp;
     operand = "/";
     output += "/";
@@ -106,6 +135,7 @@ division.addEventListener("click", () => {
 let seven = document.querySelector(".vii");
 seven.addEventListener("click", () => {
   acflag = false;
+  decflag = false;
   ac.innerHTML = bksp;
   output += "7";
   displaybox.innerHTML = output;
@@ -114,6 +144,7 @@ seven.addEventListener("click", () => {
 let eight = document.querySelector(".viii");
 eight.addEventListener("click", () => {
   acflag = false;
+  decflag = false;
   ac.innerHTML = bksp;
   output += "8";
   displaybox.innerHTML = output;
@@ -122,6 +153,7 @@ eight.addEventListener("click", () => {
 let nine = document.querySelector(".ix");
 nine.addEventListener("click", () => {
   acflag = false;
+  decflag = false;
   ac.innerHTML = bksp;
   output += "9";
   displaybox.innerHTML = output;
@@ -130,12 +162,14 @@ nine.addEventListener("click", () => {
 let multiplication = document.querySelector(".multiplication");
 multiplication.addEventListener("click", () => {
   if (opflag) {
+    operate();
     acflag = false;
-    ac.innerHTML = bksp;
+    operand = "*";
+    output += "*";
     displaybox.innerHTML = output;
   } else {
     opflag = true;
-    acflag = false;
+    decflag = true;
     ac.innerHTML = bksp;
     operand = "*";
     output += "*";
@@ -146,6 +180,7 @@ multiplication.addEventListener("click", () => {
 let four = document.querySelector(".iv");
 four.addEventListener("click", () => {
   acflag = false;
+  decflag = false;
   ac.innerHTML = bksp;
   output += "4";
   displaybox.innerHTML = output;
@@ -154,6 +189,7 @@ four.addEventListener("click", () => {
 let five = document.querySelector(".v");
 five.addEventListener("click", () => {
   acflag = false;
+  decflag = false;
   ac.innerHTML = bksp;
   output += "5";
   displaybox.innerHTML = output;
@@ -162,6 +198,7 @@ five.addEventListener("click", () => {
 let six = document.querySelector(".vi");
 six.addEventListener("click", () => {
   acflag = false;
+  decflag = false;
   ac.innerHTML = bksp;
   output += "6";
   displaybox.innerHTML = output;
@@ -170,12 +207,14 @@ six.addEventListener("click", () => {
 let subtraction = document.querySelector(".subtraction");
 subtraction.addEventListener("click", () => {
   if (opflag) {
+    operate();
     acflag = false;
-    ac.innerHTML = bksp;
+    operand = "-";
+    output += "-";
     displaybox.innerHTML = output;
   } else {
     opflag = true;
-    acflag = false;
+    decflag = true;
     ac.innerHTML = bksp;
     operand = "-";
     output += "-";
@@ -186,6 +225,7 @@ subtraction.addEventListener("click", () => {
 let one = document.querySelector(".i");
 one.addEventListener("click", () => {
   acflag = false;
+  decflag = false;
   ac.innerHTML = bksp;
   output += "1";
   displaybox.innerHTML = output;
@@ -194,6 +234,7 @@ one.addEventListener("click", () => {
 let two = document.querySelector(".ii");
 two.addEventListener("click", () => {
   acflag = false;
+  decflag = false;
   ac.innerHTML = bksp;
   output += "2";
   displaybox.innerHTML = output;
@@ -202,6 +243,7 @@ two.addEventListener("click", () => {
 let three = document.querySelector(".iii");
 three.addEventListener("click", () => {
   acflag = false;
+  decflag = false;
   ac.innerHTML = bksp;
   output += "3";
   displaybox.innerHTML = output;
@@ -210,10 +252,32 @@ three.addEventListener("click", () => {
 let addition = document.querySelector(".addition");
 addition.addEventListener("click", () => {
   if (opflag) {
-    ac.innerHTML = bksp;
+    acflag = false;
+    // operand = "+";
+    // output += "+";
+    // output.replace("+","+")
+    // output.replace("-","+")
+    // output.replace("*","+")
+    // output.replace("/","+")
+    // output.replace("%","+")
+    // operate();
+    if (
+      output[output.length - 1] == "-" ||
+      output[output.length - 1] == "*" ||
+      output[output.length - 1] == "/" ||
+      output[output.length - 1] == "%" ||
+      output[output.length - 1] == "+"
+    ) {
+      output = output.slice(0, -1);
+      output += "+";
+      displaybox.innerHTML = output;
+      return;
+    }
+    operate();
     displaybox.innerHTML = output;
   } else {
     opflag = true;
+    decflag = true;
     ac.innerHTML = bksp;
     operand = "+";
     output += "+";
@@ -238,18 +302,21 @@ darklightMode.addEventListener("click", () => {
 let zero = document.querySelector(".zero");
 zero.addEventListener("click", () => {
   acflag = false;
+  decflag = false;
   ac.innerHTML = bksp;
   output += "0";
   displaybox.innerHTML = output;
 });
 
 let decimal = document.querySelector(".decimal");
+let decflag = false;
+let decflag2 = false;
 decimal.addEventListener("click", () => {
-  if (opflag) {
+  if (decflag || decflag2) {
     ac.innerHTML = bksp;
     displaybox.innerHTML = output;
   } else {
-    opflag = true;
+    decflag = true;
     acflag = false;
     ac.innerHTML = bksp;
     output += ".";
@@ -258,13 +325,15 @@ decimal.addEventListener("click", () => {
 });
 
 let isEqualTo = document.querySelector(".isEqualTo");
-isEqualTo.addEventListener("click", () => {
+isEqualTo.addEventListener("click", operate);
+
+function operate() {
   acflag = true;
   ac.innerHTML = "AC";
+
   let args = output.split(operand);
-  arg1 = parseInt(args[0]);
-  arg2 = parseInt(args[1]);
-  operate(arg1, operand, arg2);
-  let store = output;
+  arg1 = parseFloat(args[0]);
+  arg2 = parseFloat(args[1]);
   displayAfter.innerHTML = output;
-});
+  calculate(arg1, operand, arg2);
+}
